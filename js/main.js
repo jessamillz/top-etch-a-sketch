1198f2d9b9c5f824472ac9279f3077edb9c221a9
jessamillz <34873740+jessamillz@users.noreply.github.com>
2025-01-27 12:29:08 -0500

    Add functionality to reset button and prompt user for size

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 const container = document.querySelector("#container");
 const containerWidth = container.clientWidth;
+const gridResetButton = document.querySelector("#grid-reset");
 let gridSize = 16;
+
+// Button click prompts user to input new grid size, max size 100
+// createGrid(user size) is then called
+
+gridResetButton.addEventListener('click', () => {
+    let newSize = Number(prompt("Please enter the number of squares per side, up to 100"));
+    if (Number.isInteger(newSize)) {
+        if ((newSize > 0) && (newSize <= 100)) {
+            createGrid(newSize);
+        } else {
+            alert("Please enter a number between 1 and 100.");
+        }
+    } else {
+        alert("Please enter a whole number between 1 and 100.");
+    }
+})
 
 function createGrid(size) {
     container.innerHTML = ''; // Clear existing grid
